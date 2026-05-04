aa37658fa0a92ee242b56ec0df18fce636f43716
Luca Di Salvo <luca.disalvo01@gmail.com>
2026-05-04 17:33:25 +0200

    aggiunta dei pom.xml, delle resources con i relativi application.yml e Applicazioni.java per ogni servizio

---
+++ point-of-sale-service/src/main/java/com/taasselunga/pos/PointOfSaleApplication.java
@@ -0,0 +1,12 @@
+package com.taasselunga.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointOfSaleApplication.class, args);
+    }
+}
